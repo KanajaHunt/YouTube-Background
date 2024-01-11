ab066e83524982d5ac6485530972a0338e74cd00
Kanaja Hunt <KanajaHunt@gmail.com>
2024-01-10 20:08:17 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,90 @@
+// Event listeners
+document.getElementById("purple").onclick = partyPurple;
+document.getElementById("green").onclick = partyGreen;
+document.getElementById("blue").onclick = partyBlue;
+document.getElementById("yellow").onclick = partyYellow;
+document.getElementById("brown").onclick = partyBrown;
+
+function partyPurple() {
+    document.querySelector("body").style.backgroundColor = "rgba(241,63,247,1)";
+    document.querySelector("body").style.color = "white"
+}
+function partyGreen() {
+    document.querySelector("body").style.backgroundColor = "rgba(0,253,81,1)"
+    document.querySelector("body").style.color = "white";
+}
+function partyBlue() {
+    document.querySelector("body").style.backgroundColor = "rgba(0,254,255)";
+    document.querySelector("body").style.color = "white";
+}
+function partyYellow() {
+    document.querySelector("body").style.backgroundColor = "rgba(245, 236, 39, 0.79)";
+    document.querySelector("body").style.color = "white";
+}
+function partyBrown() {
+    document.querySelector("body").style.backgroundColor = "rgba(103, 64, 23)";
+    document.querySelector("body").style.color = "white";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("purple").onclick = partyPurple;
+// document.getElementById("green").onclick = partyGreen;
+// document.getElementById("blue").onclick = partyBlue;
+// document.getElementById("yellow").onclick = partyYellow
+
+
+
+// function partyPurple() {
+//     document.querySelector("body").style.backgroundColor = "rgba(241,63,247,1)";
+//     document.querySelector("body").style.color = "white";
+// }
+
+// function partyGreen() {
+//     document.querySelector("body").style.backgroundColor = "rgba(0,253,81,1)";
+//     document.querySelector("body").style.color = "white";
+// }
+
+// function partyBlue() {
+//     document.querySelector("body").style.backgroundColor = "rgba(0,254,255)";
+//     document.querySelector("body").style.color = "white";
+// }
+
+// function partyYellow() {
+//     document.querySelector("body").style.backgroundColor = "rgba(245, 236, 39, 0.79)";
+//     document.querySelector("body").style.color = "white";
+// }
+
+// function partyBrown() {
+//     document.querySelector("body").style.backgroundColor = "rgba(103, 64, 23)";
+//     document.querySelector("body").style.color = "white";
+// }
